bbed82a53cfa65635c3c75783827b652f59420e4
Gabriel Kzk <gabriel.nasimeto34@gmail.com>
2026-04-24 20:03:56 -0300

    Add pasta de configurações

---
+++ nexo/src/app/configuracoes/configuracoes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configuracoes',
+  imports: [],
+  templateUrl: './configuracoes.html',
+  styleUrl: './configuracoes.scss',
+})
+export class Configuracoes {}
