d815b60a4c7effa9ee2d032a9b5968119c382bbe
Henrique <henripereira922@gmail.com>
2026-04-25 18:36:04 -0300

    Ajuste na estrutura de pastas: removendo nexo-angular e corrigindo para nexo

---
+++ nexo/src/app/app.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Cadastro } from './cadastro/cadastro';
+import { Dashboards } from './dashboards/dashboards';
+import { Login } from './login/login';
+import { Materias } from './materias/materias';
+import { Menu } from './menu/menu';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule, 
+    RouterOutlet, 
+    Dashboards, 
+    Materias,
+    FormsModule, 
+    Menu, 
+    Cadastro, 
+    Login],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  
+}
