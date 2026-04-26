4217b9610d4f8acafa020a64aba91d5935679e45
Gabriel Kzk <gabriel.nasimeto34@gmail.com>
2026-04-26 16:42:24 -0300

    Personalização do filtro de busca e alteração da cor do texto abaixo de Minhas Matérias

--- nexo/src/app/materias/materias.ts
+++ nexo/src/app/materias/materias.ts
@@ -25,15 +25,51 @@ export class Materias {
   buscaDeTermos: string = '';
   selecionarNivel: string = 'Todos os Níveis';
 
-  // Lista completa de matérias
+  dropdownAberto: boolean = false;
+
+  toggleDropdown() {
+    this.dropdownAberto = !this.dropdownAberto;
+  }
+
+  selecionar(valor: string) {
+    this.selecionarNivel = valor;
+    this.dropdownAberto = false;
+  }
+
   subject: Subject[] = [
-    { title: 'Biologia', level: 'Fundamental', progress: 45, completedLessons: 11, totalLessons: 24, iconClass: 'bi-dna', gradientClass: 'green-gradient', buttonClass: 'green-button' },
-    { title: 'Matemática', level: 'Médio', progress: 60, completedLessons: 22, totalLessons: 36, iconClass: 'bi-123', gradientClass: 'blue-gradient', buttonClass: 'blue-button' },
-    { title: 'Inglês', level: 'Médio', progress: 70, completedLessons: 21, totalLessons: 30, iconClass: '', iconText: 'US', gradientClass: 'pink-gradient', buttonClass: 'pink-button' },
-    // Adicione as outras materias depois 
+    {
+      title: 'Biologia',
+      level: 'Fundamental',
+      progress: 45,
+      completedLessons: 11,
+      totalLessons: 24,
+      iconClass: 'bi-dna', // 🧬
+      gradientClass: 'green-gradient',
+      buttonClass: 'green-button'
+    },
+    {
+      title: 'Matemática',
+      level: 'Médio',
+      progress: 60,
+      completedLessons: 22,
+      totalLessons: 36,
+      iconClass: 'bi-123',
+      gradientClass: 'blue-gradient',
+      buttonClass: 'blue-button'
+    },
+    {
+      title: 'Inglês',
+      level: 'Médio',
+      progress: 70,
+      completedLessons: 21,
+      totalLessons: 30,
+      iconClass: '',
+      iconText: 'US',
+      gradientClass: 'pink-gradient',
+      buttonClass: 'pink-button'
+    }
   ];
 
-  // Getter que filtra a lista em tempo real
   get filtroDeBusca() {
     return this.subject.filter(subject => {
       const matchesSearch = subject.title.toLowerCase().includes(this.buscaDeTermos.toLowerCase());
