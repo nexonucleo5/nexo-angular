c908524cb125e3860b72f7646a5f6a6984200d80
Gabriel Kzk <gabriel.nasimeto34@gmail.com>
2026-04-28 19:45:49 -0300

    Alteração menu

---
+++ nexo/src/app/menu-perfil/menu-perfil.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-perfil',
+  imports: [],
+  templateUrl: './menu-perfil.html',
+  styleUrl: './menu-perfil.scss',
+})
+export class MenuPerfil {}
