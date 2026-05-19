0e5a05345ef9537c4c0694ab8bebd88288109fc6
Gabriel Kzk <gabriel.nasimeto34@gmail.com>
2026-05-18 21:20:14 -0300

    FEAT: Criação da branch Tela de Relatórios

---
+++ nexo/src/app/relatorios-diretor/relatorios-diretor.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relatorios-diretor',
+  imports: [],
+  templateUrl: './relatorios-diretor.html',
+  styleUrl: './relatorios-diretor.scss',
+})
+export class RelatoriosDiretor {}
